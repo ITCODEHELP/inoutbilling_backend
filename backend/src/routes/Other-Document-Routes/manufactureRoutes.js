f7417ce06dc80b0516783a4f4deb3b6d1d4f11d0
Hardik Agravat <agravathardik855@gmail.com>
2026-02-12 14:34:31 +0530

    Manufacture Download, WhatsApp, Email API Done

--- backend/src/routes/Other-Document-Routes/manufactureRoutes.js
+++ backend/src/routes/Other-Document-Routes/manufactureRoutes.js
@@ -6,9 +6,17 @@ const {
     getManufactureById,
     updateManufacture,
     searchManufactures,
-    deleteManufacture
+    deleteManufacture,
+    downloadManufacturePDF,
+    shareManufactureEmail,
+    shareManufactureWhatsApp,
+    generateManufacturePublicLink,
+    viewPublicManufacture
 } = require('../../controllers/Other-Document-Controller/manufactureController');
 const { protect } = require('../../middlewares/authMiddleware');
+
+// Public Link (Unprotected)
+router.get('/view-public/:id/:token', viewPublicManufacture);
 
 router.use(protect);
 
@@ -22,5 +30,14 @@ router.route('/:id')
     .get(getManufactureById)
     .put(updateManufacture)
     .delete(deleteManufacture);
+
+// PDF and Sharing
+
+router.get('/:id/download-pdf', downloadManufacturePDF);
+router.post('/:id/share-email', shareManufactureEmail);
+router.post('/:id/share-whatsapp', shareManufactureWhatsApp);
+router.get('/:id/public-link', generateManufacturePublicLink);
+
+
 
 module.exports = router;
