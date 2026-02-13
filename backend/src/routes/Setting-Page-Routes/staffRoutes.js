623d84de78413eb325011fbd8cce6a556b21d5df
Hardik Agravat <agravathardik855@gmail.com>
2026-02-13 12:43:09 +0530

    Staff Account Done

--- backend/src/routes/Setting-Page-Routes/staffRoutes.js
+++ backend/src/routes/Setting-Page-Routes/staffRoutes.js
@@ -3,12 +3,21 @@ const router = express.Router();
 const {
     createStaff,
     getStaffByName,
-    getAllStaff
+    getAllStaff,
+    updateStaff,
+    deleteStaff,
+    staffLogin
 } = require('../../controllers/Setting-Page-Controller/staffController');
 const { protect } = require('../../middlewares/authMiddleware');
 
+// Owner-protected routes
 router.post('/create', protect, createStaff);
 router.get('/all', protect, getAllStaff);
 router.get('/search/:name', protect, getStaffByName);
+router.put('/:id', protect, updateStaff);
+router.delete('/:id', protect, deleteStaff);
+
+// Public staff login route (does not use owner auth)
+router.post('/login', staffLogin);
 
 module.exports = router;
